0bd203d90207238ab8c084958a3c1465fc0fe806
nuno69 <nuno69a@gmail.com>
2022-10-02 16:16:31 +0200

    Initial Commit

---
+++ Tyflocentrum/TyflocentrumApp.swift
@@ -0,0 +1,17 @@
+//
+//  TyflocentrumApp.swift
+//  Tyflocentrum
+//
+//  Created by Arkadiusz Świętnicki on 02/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct TyflocentrumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
